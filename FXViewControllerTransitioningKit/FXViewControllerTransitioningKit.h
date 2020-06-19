e3c42aa3a3ad293c4e26c854e12237bb2c2e8a5d
feixue299 <ariablink299@gmail.com>
2020-06-19 14:14:42 +0800

    Initial Commit

---
+++ FXViewControllerTransitioningKit/FXViewControllerTransitioningKit.h
@@ -0,0 +1,19 @@
+//
+//  FXViewControllerTransitioningKit.h
+//  FXViewControllerTransitioningKit
+//
+//  Created by 8-PC on 2020/6/19.
+//  Copyright © 2020 wu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FXViewControllerTransitioningKit.
+FOUNDATION_EXPORT double FXViewControllerTransitioningKitVersionNumber;
+
+//! Project version string for FXViewControllerTransitioningKit.
+FOUNDATION_EXPORT const unsigned char FXViewControllerTransitioningKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FXViewControllerTransitioningKit/PublicHeader.h>
+
+
